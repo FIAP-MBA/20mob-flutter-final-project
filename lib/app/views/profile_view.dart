05a28bb8271cec53b7dd9fbe93b06d68ca8915ad
Welton Dornelas <dornelas.mag@gmail.com>
2021-05-02 09:17:21 -0300

    refact some view and adding new views

---
+++ lib/app/views/profile_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ProfileView extends StatefulWidget {
+  @override
+  _ProfileViewState createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first screen when tapped.
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
