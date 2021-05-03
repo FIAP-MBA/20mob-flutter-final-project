34c8cc8a8d1ba3d5e2712719e4959caebd0d6914
Welton Dornelas <dornelas.mag@gmail.com>
2021-05-03 20:20:46 -0300

    update local storage refresh

--- lib/app/bloc/profile_bloc.dart
+++ lib/app/bloc/profile_bloc.dart
@@ -1,9 +1,27 @@
+import 'package:flutter_20mob_project_final/app/bloc/profile_controller.dart';
+import 'package:flutter_20mob_project_final/app/models/profile_model.dart';
+import 'package:flutter_20mob_project_final/app/repositories/profile_repository.dart';
+
 class ProfileBloc {
-
-  _getLocalProfile() async {
+  ProfileRepository repository;
+  getLocalProfile() async {
     try {
+      ProfileModel profile = await repository.getProfile();
+      if(profile == null) {
+        profile = ProfileModel(1, "Insira seu nome", "Insira seu email");
+      }
+      ProfileController.instance.changeProfile(profile);
+      print(profile);
+    } catch (error) {
+      print(error);
+    }
+  }
 
-    } catch(error) {
+  insertLocalProfile(String name, String email, int position) async {
+    try {
+      ProfileModel profile = ProfileModel(position, name, email);
+      repository.insertProfile(profile);
+    } catch (error) {
       print(error);
     }
   }
--- lib/app/views/profile_form_view.dart
+++ lib/app/views/profile_form_view.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_20mob_project_final/app/bloc/profile_bloc.dart';
+import 'package:flutter_20mob_project_final/app/bloc/profile_controller.dart';
+import 'package:flutter_20mob_project_final/app/repositories/db/app_database.dart';
+import 'package:flutter_20mob_project_final/app/repositories/profile_repository.dart';
 
 class ProfileFormView extends StatefulWidget {
-
   @override
   _ProfileFormViewState createState() => _ProfileFormViewState();
 }
 
 class _ProfileFormViewState extends State<ProfileFormView> {
-
   @override
   void initState() {
     super.initState();
+    _createDataBase();
+  }
 
+  _createDataBase() {
+    $FloorAppDatabase.databaseBuilder('app_database.db').build().then((value) {
+      AppDatabase _database = value;
+      profileBloc.repository = ProfileRepository(_database);
+      profileBloc.getLocalProfile();
+    });
   }
 
   @override
@@ -27,15 +37,19 @@ class _ProfileFormViewState extends State<ProfileFormView> {
     return Column(
       children: [
         UserAccountsDrawerHeader(
-          currentAccountPicture: ClipRRect(
-            borderRadius: BorderRadius.circular(40),
-            child: Image.network(
-                "https://media-exp1.licdn.com/dms/image/C4D03AQHhzonVPmrkow/profile-displayphoto-shrink_800_800/0/1612361529978?e=1625097600&v=beta&t=mjnF-EARrM-7HJo4YG_HOf7vphgiDhYRSThJhU1JS0U"),
-          ),
-          accountName: Text("Rafael"),
-          accountEmail: Text("t@t.com"),
-        ),
-        Padding(padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            currentAccountPicture: ClipRRect(
+              borderRadius: BorderRadius.circular(40),
+              child: Image.network(
+                  "https://media-exp1.licdn.com/dms/image/C4D03AQHhzonVPmrkow/profile-displayphoto-shrink_800_800/0/1612361529978?e=1625097600&v=beta&t=mjnF-EARrM-7HJo4YG_HOf7vphgiDhYRSThJhU1JS0U"),
+            ),
+            accountName: Text(ProfileController.instance.profile != null
+                ? ProfileController.instance.profile.name
+                : ""),
+            accountEmail: Text(ProfileController.instance.profile != null
+                ? ProfileController.instance.profile.email
+                : "")),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
           child: TextFormField(
             decoration: InputDecoration(
               icon: Icon(Icons.person),
@@ -43,7 +57,8 @@ class _ProfileFormViewState extends State<ProfileFormView> {
             ),
           ),
         ),
-        Padding(padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
           child: TextFormField(
             decoration: InputDecoration(
               icon: Icon(Icons.email),
@@ -51,17 +66,15 @@ class _ProfileFormViewState extends State<ProfileFormView> {
             ),
           ),
         ),
-        Padding(padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-          child: ElevatedButton(
-            child: Text('Salvar'),
-            style: ElevatedButton.styleFrom(
-              primary: Colors.teal,
-            ),
-            onPressed: () {
-            },
-
-          )
-        ),
+        Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            child: ElevatedButton(
+              child: Text('Salvar'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.teal,
+              ),
+              onPressed: () {},
+            )),
       ],
     );
   }
